038a815cd1fcf3487c9fe075515a5d24e2803bcf
alam7865 <sabbajalam5@gmail.com>
2025-06-09 23:52:40 +0530

    Practise python

---
+++ Prac/prac.py
@@ -0,0 +1,57 @@
+# x=["apple","mango","banana"]
+# y=["apple","mango","banana"]
+# if(x == y):
+#     print("Hello");
+# else:
+#     print("World")    
+
+#///////////////////////////////////////////
+# print(9**0.5)
+
+#///////////////// List /////////////////////
+
+# x=["apple","mango","orange"]
+# # print("This is my List", x)
+
+# for i in x:
+#     str=i;
+#     print("\n")
+#     for b in str:
+#         print(b)
+
+# /////////////////////////////////////////////
+# x="Sabaz";
+# print(x[1])
+
+#////////////////////////////////////////////
+# x=["apple","mango","orange"]
+# x.append("kiwi");
+# print(len(x))
+
+#//////////////////////////////////////////////////
+
+# thislist=list(("Orange","Mango","Banana"));
+# print(thislist)
+
+
+#//////////////////////////////////////////////////
+# list=["apple","banana","cherry","orange","Kiwi","mango"]
+# # print(list[:4])   # ['apple', 'banana', 'cherry', 'orange']
+# # print(list[1:3])
+# list.append("Sabaz");
+# list.insert(0,"Hello World")
+# print(list);
+
+#////////////////////////////////////////////////////
+# x=["Apple","mango","Apple"]
+# y=["Orange","Banana"]
+# # x.append(y) # ['Apple', 'mango', ['Orange', 'Banana']]
+# x.extend(y);  # ['Apple', 'mango', 'Orange', 'Banana']
+# print(x)
+# x.remove("Apple")
+# print(x)
+
+# ///////////////////////////////////////////////////
+x=["Apple","mango","Apple"]
+x.pop(2);
+print(x)
