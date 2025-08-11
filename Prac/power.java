d75b5649cbd44a58483c6cb235cf29b210a2c0d1
alam7865 <sabbajalam5@gmail.com>
2025-08-11 14:30:47 +0530

    Prac

---
+++ Prac/power.java
@@ -0,0 +1,23 @@
+// import java.util.*;
+
+// public class power {
+//     public static void main(String[] args) {
+//         int n = 16;
+
+//         while (n > 1) {
+//             n = n / 2;
+//             System.out.println(n);
+//         }
+
+//         System.out.println(n);
+//     }
+// }
+
+select
+extract(year from order_date) as Year,
+to_char(order_date,'Mon') as Month,
+sum(sales)
+from t1
+group by
+1,2
+order by 2
